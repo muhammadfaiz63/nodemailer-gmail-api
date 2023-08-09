43426ea2a2903b5c58a4c24e05e70abcb102a79a
muhammad faiz <muhammadfaiz7130@gmail.com>
2023-08-09 22:39:38 +0700

    google auth

--- server.js
+++ server.js
@@ -7,10 +7,20 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const axios = require('axios')
 const multer = require("multer");
-const fs = require('fs');
 const path = require("path");
+const fs = require('fs').promises;
+const process = require('process');
+const {authenticate} = require('@google-cloud/local-auth');
+const {google} = require('googleapis');
 
 var app = express();
+
+const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = path.join(process.cwd(), 'token.json');
+const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 const storage = multer.diskStorage({
   destination: "./repo/pdf",
@@ -74,6 +84,90 @@ app.use(bodyParserJsonMiddleware());
 
 app.use(logger("dev"));
 
+async function loadSavedCredentialsIfExist() {
+  try {
+    const content = await fs.readFile(TOKEN_PATH);
+    const credentials = JSON.parse(content);
+    return google.auth.fromJSON(credentials);
+  } catch (err) {
+    return null;
+  }
+}
+
+/**
+ * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
+ *
+ * @param {OAuth2Client} client
+ * @return {Promise<void>}
+ */
+async function saveCredentials(client) {
+  const content = await fs.readFile(CREDENTIALS_PATH);
+  const keys = JSON.parse(content);
+  const key = keys.installed || keys.web;
+  const payload = JSON.stringify({
+    type: 'authorized_user',
+    client_id: key.client_id,
+    client_secret: key.client_secret,
+    refresh_token: client.credentials.refresh_token,
+  });
+  await fs.writeFile(TOKEN_PATH, payload);
+}
+
+/**
+ * Load or request or authorization to call APIs.
+ *
+ */
+async function authorize() {
+  let client = await loadSavedCredentialsIfExist();
+  if (client) {
+    return client;
+  }
+  client = await authenticate({
+    scopes: SCOPES,
+    keyfilePath: CREDENTIALS_PATH,
+  });
+  if (client.credentials) {
+    await saveCredentials(client);
+  }
+  return client;
+}
+
+/**
+ * Lists the labels in the user's account.
+ *
+ * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
+ */
+async function listLabels(auth) {
+  const gmail = google.gmail({version: 'v1', auth});
+  const res = await gmail.users.labels.list({
+    userId: 'me',
+  });
+  const labels = res.data.labels;
+  if (!labels || labels.length === 0) {
+    console.log('No labels found.');
+    return;
+  }
+  console.log('Labels:');
+  labels.forEach((label) => {
+    console.log(`- ${label.name}`);
+  });
+}
+
+app.get('/gmail', (req, res) => {
+  authorize().then(listLabels).catch((err)=>{
+    console.error(err)
+    res.json(err);
+  });
+});
+
+app.get('/getprofilegmail', (req, res) => {
+  let userid = "muhammadfaiz7130@gmail.com"
+  axios.get(`https://gmail.googleapis.com/gmail/v1/users/${userid}/profile`).then(response => {
+    res.json(response)
+  }).catch((err) => {
+    res.json(err)
+  })
+})
 
 app.get("/", (req, res) => {
   res.send("Gateway Service is Connected");
@@ -81,7 +175,7 @@ app.get("/", (req, res) => {
 
 app.use('/repo', express.static(path.join(__dirname, 'repo')))
 
-const port = process.env.PORT || 1112;
+const port = process.env.PORT || 1113;
 
 var server = require("http").createServer(app);
 
