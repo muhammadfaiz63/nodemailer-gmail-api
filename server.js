4b4ddf1cea036a5f6889e1a7bf40ad73baa57b3f
muhammad faiz <muhammadfaiz7130@gmail.com>
2023-08-16 10:27:06 +0700

    add google api

--- server.js
+++ server.js
@@ -12,15 +12,23 @@ const fs = require('fs').promises;
 const process = require('process');
 const {authenticate} = require('@google-cloud/local-auth');
 const {google} = require('googleapis');
+const {
+  OAuth2Client,
+} = require('google-auth-library');
 
 var app = express();
 
-const SCOPES = ['https://www.googleapis.com/auth/gmail.readonly'];
+const SCOPES = ["https://mail.google.com/",
+"https://www.googleapis.com/auth/gmail.addons.current.message.action",
+"https://www.googleapis.com/auth/gmail.addons.current.message.metadata",
+"https://www.googleapis.com/auth/gmail.addons.current.message.readonly",
+"https://www.googleapis.com/auth/gmail.metadata",
+"https://www.googleapis.com/auth/gmail.modify",
+"https://www.googleapis.com/auth/gmail.readonly"
+];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = path.join(process.cwd(), 'token.json');
-const CREDENTIALS_PATH = path.join(process.cwd(), 'credentials.json');
 
 const storage = multer.diskStorage({
   destination: "./repo/pdf",
@@ -34,6 +42,13 @@ const upload = multer({
   storage: storage,
   limits: { fileSize: 1000000 },
 }).single("document")
+
+const oAuth2Client = new OAuth2Client(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  'postmessage',
+  // SCOPES
+);
 
 app.use(cors());
 
@@ -84,81 +99,22 @@ app.use(bodyParserJsonMiddleware());
 
 app.use(logger("dev"));
 
-async function loadSavedCredentialsIfExist() {
-  try {
-    const content = await fs.readFile(TOKEN_PATH);
-    const credentials = JSON.parse(content);
-    return google.auth.fromJSON(credentials);
-  } catch (err) {
-    return null;
-  }
-}
-
-/**
- * Serializes credentials to a file compatible with GoogleAUth.fromJSON.
- *
- * @param {OAuth2Client} client
- * @return {Promise<void>}
- */
-async function saveCredentials(client) {
-  const content = await fs.readFile(CREDENTIALS_PATH);
-  const keys = JSON.parse(content);
-  const key = keys.installed || keys.web;
-  const payload = JSON.stringify({
-    type: 'authorized_user',
-    client_id: key.client_id,
-    client_secret: key.client_secret,
-    refresh_token: client.credentials.refresh_token,
-  });
-  await fs.writeFile(TOKEN_PATH, payload);
-}
-
-/**
- * Load or request or authorization to call APIs.
- *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
-
-/**
- * Lists the labels in the user's account.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-async function listLabels(auth) {
-  const gmail = google.gmail({version: 'v1', auth});
-  const res = await gmail.users.labels.list({
-    userId: 'me',
-  });
-  const labels = res.data.labels;
-  if (!labels || labels.length === 0) {
-    console.log('No labels found.');
-    return;
-  }
-  console.log('Labels:');
-  labels.forEach((label) => {
-    console.log(`- ${label.name}`);
-  });
-}
-
-app.get('/gmail', (req, res) => {
-  authorize().then(listLabels).catch((err)=>{
-    console.error(err)
-    res.json(err);
-  });
+app.post('/auth/google', async (req, res) => {
+  const { tokens } = await oAuth2Client.getToken(req.body.code); // exchange code for tokens
+  console.log(tokens);
+  
+  res.json(tokens);
 });
+
+app.post('/auth/google/refresh-token', async (req, res) => {
+  const user = new UserRefreshClient(
+    clientId,
+    clientSecret,
+    req.body.refreshToken,
+  );
+  const { credentials } = await user.refreshAccessToken(); // optain new tokens
+  res.json(credentials);
+})
 
 app.get('/getprofilegmail', (req, res) => {
   let userid = "muhammadfaiz7130@gmail.com"
